bd5f74703699a0cffa24770e1fd9f64592e492ff
John J. Jordan <jj@jjjordan.io>
2017-10-19 06:27:11 +0000

    Fill in some tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,20 @@ const gulp = require('gulp'),
       hg = require('gulp-hg'),
       version = require('./version'),
       lock = require('gulp-lock'),
+      rename = require('gulp-rename'),
       utils = require('./utils');
 
+// Lock for tasks that use the Mercurial repository
 var hgLock = lock();
+
+// Task to check out or update JOE from Mercurial
+gulp.task('get-joe', function(done) {
+    if (!fs.existsSync('./joe')) {
+        hg.clone('http://hg.code.sf.net/p/joe-editor/mercurial', './joe', function() { done(); });
+    } else {
+        hg.pull({cwd: './joe'}, function() { done(); });
+    }
+});
 
 // Gather each version by checking out repo to the commit listed in versions.yml,
 // then copying markdown files into intermediate/md
@@ -16,7 +27,7 @@ version.versions.forEach(function (v) {
           return done();
         }
         
-        return hg.update(version.info[v].tags.unix, {cwd: './joe'}, function(out, err) {
+        return hg.update(version.info[v].tags.unix, {cwd: './joe'}, (out, err) => {
             gulp.src(['joe/README.md', 'joe/NEWS.md', 'joe/docs/man.md', 'joe/docs/hacking.md'])
                 .pipe(gulp.dest('intermediate/md/' + v))
                 .on('end', done);
@@ -30,7 +41,7 @@ version.versions.forEach(function (v) {
             return done();
         }
         
-        return hg.update(version.info[v].tags.windows, {cwd: './joe'}, function(out, err) {
+        return hg.update(version.info[v].tags.windows, {cwd: './joe'}, (out, err) => {
             gulp.src(['joe/docs/windows.md'])
                 .pipe(gulp.dest('intermediate/md/' + v))
                 .on('end', done);
@@ -38,28 +49,62 @@ version.versions.forEach(function (v) {
     }));
 });
 
+// Get documents at the tip version (for changelog and README)
+gulp.task('gather-tip', hgLock.cb((done) => {
+    return hg.update({cwd: './joe'}, (out, err) => {
+        gulp.src(['joe/README.md', 'joe/NEWS.md', 'joe/docs/man.md', 'joe/docs/hacking.md'])
+            .pipe(gulp.dest('intermediate/md/tip/'))
+            .on('end', done);
+    });
+}));
+
 // Depend on all gather-* tasks above.
 gulp.task('gather', version.versions.map((v) => 'gather-unix-' + v).concat(version.versions.map((v) => 'gather-windows-' + v)));
 
 // Render all man.md's with marked against templates/manual.ejs --> put in intermediate/dist/<version>/man.html
-gulp.task('manuals', ['gather'], function() {
+gulp.task('md:manuals', ['gather'], () => {
     return gulp.src(['intermediate/md/**/man.md'], {base: './intermediate/md'})
                .pipe(utils.convertmd(gulp.src('templates/manual.ejs')))
                .pipe(gulp.dest('intermediate/dist'));
 });
 
-// Render news.md with marked against news.mustache --> put in intermediate/dist/<version>/news.md.html
-gulp.task('news', ['gather'], function() {
-    
+// Render news.md with marked against news.mustache --> put in intermediate/dist/<version>/NEWS.html
+gulp.task('md:news', ['gather-tip'], () => {
+    return gulp.src(['intermediate/md/tip/NEWS.md'])
+               .pipe(utils.convertmd(gulp.src('templates/news.ejs')))
+               .pipe(gulp.dest('intermediate/dist'));
 });
 
-// Task to check out or update JOE from Mercurial
-gulp.task('get-joe', function(done) {
-    if (!fs.existsSync('./joe')) {
-        hg.clone('http://hg.code.sf.net/p/joe-editor/mercurial', './joe', function() { done(); });
-    } else {
-        hg.pull({cwd: './joe'}, function() { done(); });
-    }
+// Render news.md for each version, splitting out only that version's changes --> put in intermediate/dist/<version>/index.html
+gulp.task('md:releases', ['gather'], () => {
+    return gulp.src(['intermediate/md/**/NEWS.md'], {base: './intermediate/md'})
+               .pipe(utils.convertmd(gulp.src('templates/release.ejs')))
+               .pipe(rename({basename: "index"}))
+               .pipe(gulp.dest('intermediate/dist'));
 });
 
-gulp.task('default', ['manuals']);
+// Render hacking guide for the tip version --> put in intermediate/dist/hacking.html
+gulp.task('md:hacking', ['gather-tip'], () => {
+    return gulp.src(['intermediate/md/tip/hacking.md'])
+               .pipe(utils.convertmd(gulp.src('templates/hacking.ejs')))
+               .pipe(gulp.dest('intermediate/dist'));
+});
+
+// Render index from the tip's README --> put in intermediate/dist/index.html
+gulp.task('md:index', ['gather-tip'], () => {
+    return gulp.src(['intermediate/md/tip/README.md'])
+               .pipe(utils.convertmd(gulp.src('templates/index.ejs')))
+               .pipe(rename("index.html"))
+               .pipe(gulp.dest('intermediate/dist'));
+});
+
+// Task that groups all markdown conversion
+gulp.task('markdown', ['md:manuals', 'md:news', 'md:releases', 'md:hacking', 'md:index']);
+
+// Task that copies images from img --> dist/img
+gulp.task('images', () => {
+    return gulp.src(['img/*'], {read: false}).pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('default', ['markdown']);
