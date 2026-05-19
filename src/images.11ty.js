aca6d9c1c07d70324c2ec385b27a94d6666a8d7b
John J. Jordan <jj@jjjordan.io>
2026-05-18 21:51:26 -0700

    Add bundles, start trying to get the right assets in place. Replace the layouts with Nunjucks.

--- src/images.11ty.js
+++ src/images.11ty.js
@@ -1,55 +1,44 @@
-import fs from 'node:fs/promises';
-import nodefs from 'node:fs';
+import fs from 'node:fs';
 import git from 'isomorphic-git';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
-import http from 'isomorphic-git/http/node';
 
 const GITPATH = '../joe-git';
+const fileDir = path.dirname(fileURLToPath(import.meta.url));
+const dir = path.resolve(fileDir, GITPATH);
+
+// Copies all images out of git main and into an img/ directory in the output.
 
 export default class GitImageGenerator {
-  async data() {
-    const fileDir = path.dirname(fileURLToPath(import.meta.url));
-    const dir = path.resolve(fileDir, GITPATH);
+    async data() {
+        const oid = await git.resolveRef({ fs, dir, ref: "main" });
+        const files = await git.listFiles({ fs, dir, oid });
 
-    const oid = await git.resolveRef({ fs, dir, ref: "main" });
-    const files = await git.listFiles({ fs, dir, oid });
+        // Filter for all images
+        const images = files.filter(f => f.match(/\.(jpg|jpeg|png|gif|svg)$/i));
 
-    // Filter for all images
-    const images = files.filter(f => f.match(/\.(jpg|jpeg|png|gif|svg)$/i));
+        let allImages = images.map(filePath => {
+            return {
+                oid,
+                filePath,
+                // Extract filename to use in permalink
+                name: path.basename(filePath)
+            };
+        });
 
-    let allImages = [];
-    images.forEach(filePath => {
-      allImages.push({
-        oid,
-        filePath,
-        // Extract filename to use in permalink
-        name: path.basename(filePath)
-      });
-    });
+        return {
+            pagination: {
+                data: "allImages",
+                size: 1,
+                alias: "image"
+            },
+            allImages,
+            permalink: (data) => `img/${data.image.name}`
+        };
+    }
 
-    return {
-      pagination: {
-        data: "allImages",
-        size: 1,
-        alias: "image"
-      },
-      allImages,
-      permalink: (data) => `img/${data.image.name}`
-    };
-  }
-
-  async render(data) {
-    const fileDir = path.dirname(fileURLToPath(import.meta.url));
-    const dir = path.resolve(fileDir, GITPATH);
-
-    const { blob } = await git.readBlob({
-      fs: nodefs,
-      dir,
-      oid: data.image.oid,
-      filepath: data.image.filePath
-    });
-
-    return Buffer.from(blob);
-  }
+    async render(data) {
+        const { blob } = await git.readBlob({ fs, dir, oid: data.image.oid, filepath: data.image.filePath });
+        return Buffer.from(blob);
+    }
 }
